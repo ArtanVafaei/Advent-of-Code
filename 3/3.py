9f200616ae5ce182e16c24556eef06adf8fe1112
Artan Vafaei <artan.vafaei@yahoo.com>
2024-12-27 17:42:50 -0600

    2024 Day 3

--- 3/3.py
+++ 3/3.py
@@ -8,15 +8,15 @@ p1 = 0
 p2 = 0
 enabled = True
 
-for a, b, do, dont in findall(r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))", D):
+for x, y, do, dont in findall(r"mul\((\d+),(\d+)\)|(do\(\))|(don't\(\))", D):
     if do:
         enabled = True
     elif dont:
         enabled = False
     else:
-        p1 += int(a) * int(b)
+        p1 += int(x) * int(y)
         if enabled:
-            p2 += int(a) * int(b)
+            p2 += int(x) * int(y)
 
 print(p1)
 print(p2)
